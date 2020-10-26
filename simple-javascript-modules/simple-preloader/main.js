22ebccbb0efcd42c0343308eabb065376016529e
Omar Masari <omaronweb@gmail.com>
2020-10-26 22:18:26 +0200

    moved folder

--- simple-javascript-modules/simple-preloader/main.js
+++
@@ -1,9 +0,0 @@
-let preloaderGroup = document.getElementById("preloaderGroup");
-window.addEventListener("load", vanish);
-
-function vanish() {
-    preloaderGroup.classList.add("vanish");
-    setTimeout(() => {
-        preloaderGroup.style.display = "none";
-    }, 500);
-}
